7eb85c4691ae444ec475f3a5d3f69940d7738d97
artegful <artemklimo@gmail.com>
2021-06-14 15:18:55 +0300

    add extentions and fixes from laba4 to laba3

--- laba3/note/views.py
+++ laba3/note/views.py
@@ -5,17 +5,18 @@ from django.utils import timezone
 from .models import Note, NoteForm
 from django.shortcuts import get_object_or_404
 
+
 def index(request):
     if request.user.is_authenticated:
-        notes = Note.objects.filter(user__exact=request.user).order_by('create_time')
+        notes = Note.objects.filter(user__exact=request.user).order_by("create_time")
 
-        return render(request, 'note/notes_list.html', {'notes': notes})
+        return render(request, "note/notes_list.html", dict(notes=notes))
     else:
-        return redirect('authentication/login')
+        return redirect("authentication/login")
 
 
 def add(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = NoteForm(request.POST)
 
         if form.is_valid():
@@ -27,30 +28,30 @@ def add(request):
             note.update_time = now
             note.save()
 
-            return redirect('index')
+            return redirect("index")
         else:
             messages.error(request, "note is not valid")
 
     form = NoteForm()
-    return render(request, 'note/add_note.html', {'form': form})
+    return render(request, "note/add_note.html", dict(form=form))
 
 
 def delete(request, pk):
     note = get_object_or_404(Note, pk=pk)
     if note.user != request.user:
-        messages.error(request, 'You are not authenticated to perform this action')
+        messages.error(request, "You are not authenticated to perform this action")
     else:
         note.delete()
 
-    return redirect('index')
+    return redirect("index")
 
 
 def edit(request, pk):
     note = get_object_or_404(Note, pk=pk)
     if note.user != request.user:
-        messages.error(request, 'You are not authenticated to perform this action')
-        return redirect('index')
-    if request.method == 'POST':
+        messages.error(request, "You are not authenticated to perform this action")
+        return redirect("index")
+    if request.method == "POST":
         form = NoteForm(request.POST, instance=note)
         if form.is_valid():
             note = form.save(commit=False)
@@ -60,10 +61,9 @@ def edit(request, pk):
             note.update_time = now
             note.save()
 
-            return redirect('index')
+            return redirect("index")
     else:
-        form = NoteForm(initial={
-            'title': note.title,
-            'content': note.content
-        }, instance=note)
-        return render(request, 'note/add_note.html', {'form': form})
+        form = NoteForm(
+            initial=dict(title=note.title, content=note.content), instance=note
+        )
+        return render(request, "note/add_note.html", dict(form=form))
--- laba3/noty/wsgi.py
+++ laba3/noty/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'noty.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "noty.settings")
 
 application = get_wsgi_application()
